979b98c2848f5d727f698a96dd82fba1068066b3
wverastegui <wudmir@gmail.com>
2022-12-06 14:01:28 +0100

    Update files

--- codes/convert.py
+++ codes/convert.py
@@ -4,9 +4,10 @@ def read_file(format, inputfilename):
     mol = next(pybel.readfile(format, inputfilename))
     return (mol)
 
-def convert(mol):
+def convert(format_in, inputfilename):
+    mol = read_file(format_in, inputfilename)
     mol.make3D()
-    #mol.OBMol.AddHydrogens()
+    mol.OBMol.NumAtoms()
     return(mol)
 
 def write_file(mol, format, outfilename):
@@ -17,8 +18,8 @@ if __name__ == "__main__":
     format_out = "xyz"
     inputfilename = "sample.smi"
     outfilename = "sample_conv.xyz"
-    mol = read_file(format_in, inputfilename)
-    mol = convert(mol)
+    
+    mol = convert(format_in, inputfilename)
     write_file(mol, format_out, outfilename)
 
 
