13589c594c27b4d1dbb789098a34f709c15a0f54
wverastegui <wudmir@gmail.com>
2022-12-12 19:32:43 +0100

    Updated script adn added plots

--- codes/extract_class_features.py
+++ codes/extract_class_features.py
@@ -13,16 +13,15 @@ def extract_metadata(mol_list, group_str):
     data = []
     mol_name = []
     smile = []
-    group_type = []
+    GROUP_TYPE = []
     n_at = []
     for line in mol_list:
         data.append(line.data)
         mol_name.append(line.data["NAME"])
-        group_type.append(line.data[group_str])
+        GROUP_TYPE.append(line.data[group_str])
         smile.append(line.data["SMILES"])
         n_at.append(line.OBMol.NumAtoms())
-
-    df = pd.DataFrame([mol_name, n_at, group_type, smile])
+    df = pd.DataFrame([mol_name, n_at, GROUP_TYPE, smile])
     pd.set_option('display.colheader_justify', 'center') 
     return (df)
 
@@ -30,39 +29,36 @@ def extract_metadata(mol_list, group_str):
 def formating_dataframe(df, set_idx=False):
     # False for default index
     df = df.transpose()
-    df.columns = ["Name", "N_at", "Group_type", "Smile"]
+    df.columns = ["Name", "N_at", "GROUP_TYPE", "Smile"]
     df = df.sort_values(by=['N_at'], ascending=False)
     if set_idx is True:
-        df = df.set_index("Group_type")
+        df = df.set_index("GROUP_TYPE")
     return (df)
 
 
-def arrange_dataframe_pivot(df):
-    pivot = df.pivot(index="Name", columns="Group_type", values="N_at")
-    pivot.index.name = "Name"
-    pivot.columns.name = "Group_type"
-    return (pivot)
+# def arrange_dataframe_pivot(df):
+#     pivot = df.pivot(index="Name", columns="GROUP_TYPE", values="N_at")
+#     pivot.index.name = "Name"
+#     pivot.columns.name = "GROUP_TYPE"
+#     return (pivot)
 
 
-def clustering_and_statistics(df, group_type, stats=False):
-    grouped = df.groupby(group_type)
+def clustering_and_statistics(df, GROUP_TYPE, stats=False):
+    grouped = df.groupby(GROUP_TYPE)
     df_col = grouped[["N_at"]]
-    
     size_df = grouped.size().to_frame(name="N_item/grp")
     max_at = df_col.max()
-    df_merge = size_df.merge(max_at, how='inner', on='Group_type')
+    df_merge = size_df.merge(max_at, how='inner', on='GROUP_TYPE')
+    df_merge = df_merge.sort_values(by=['N_item/grp'], ascending=[False])
     df_merge.rename(columns = {'N_at':'Max_n_at'}, inplace = True)
     max_at.rename(columns = {'N_at':'Max_n_at'}, inplace = True)
-
+    print("Number of groups found --> ", grouped.ngroups)
     if stats is True:
-        print("Number of groups found --> ", grouped.ngroups)
         print(grouped.size())
         print(max_at)
- 
     grp_list = []
     for group in grouped:
         grp_list.append(group)
-    
     return (grp_list, df_merge)
 
 
@@ -73,33 +69,37 @@ def full_formated_infputfile(format, file, group_str):
     return (df)
 
 
+def plot_stats(df, plotname, col_name1, col_name2=None):
+    if col_name2 is None:
+        df.plot.bar(y=col_name1, use_index=True)
+        plt.tick_params(left = True, right = False , labelleft = True ,
+        labelbottom = False, bottom = True) 
+        plt.xlabel("Groups")
+        plt.xticks(rotation=30, horizontalalignment="center")
+        plt.savefig(plotname +'.png')
+    else:
+        df.plot.bar(y=[col_name1, col_name2], use_index=True)
+        plt.tick_params(left = True, right = False , labelleft = True ,
+        labelbottom = False, bottom = True) 
+        plt.xlabel("Groups")
+        plt.xticks(rotation=30, horizontalalignment="center")
+        plt.savefig(plotname +'.png')
+        
+
 if __name__ == "__main__":
-    #file = "RECETOX_GC-EI-MS_20201028.sdf"
-    file = "sample_test.sdf"
+    file = "RECETOX_GC-EI-MS_20201028.sdf"
+    #file = "sample_test.sdf"
     format = "sdf"
     # group_str: Kingdom, Superclass, Class, Subclass, Parent Level 1, Parent Level 2, Parent Level 3
-    group_str = "Class"
+    group_str = "Subclass"
     my_df = full_formated_infputfile(format, file, group_str)
-    # arrange_dataframe_pivot(my_df)  only works for default set of index  in formating_dataframe()
-    # my_df = arrange_dataframe_pivot(my_df) 
-    grp_list, grp_stat = clustering_and_statistics(my_df, group_type="Group_type", stats=False)
+    grp_list, grp_stat = clustering_and_statistics(my_df, GROUP_TYPE="GROUP_TYPE", stats=False)
     print(grp_stat)
-    #print(grp_list)
+    #plot_stats(grp_stat, "plot1", "N_item/grp")
+    #plot_stats(grp_stat, "plot2", "Max_n_at")
+    plot_stats(grp_stat, "plot_Subclass", "N_item/grp", "Max_n_at")
 
 
-
-# df.plot.bar(y="n_at", use_index=True)
-# pivot.plot.bar()
-# plt.savefig('test.png')
-
-
-#df = df['n_at'].sort_values()
-#df = df.groupby('class').groups
-
-#print(df)
-#print(df.get_group('Fatty Acyls'))
-#print(df.get_group('Phenanthrenes and derivatives'))
-
-    
+ 
     
 
