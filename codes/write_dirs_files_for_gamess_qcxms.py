802403aa0bf1f7dd4e013091e21c76a31bc17c15
wverastegui <wudmir.rojas@recetox.muni.cz>
2023-05-09 15:52:02 +0200

    added funcs

--- codes/write_dirs_files_for_gamess_qcxms.py
+++ codes/write_dirs_files_for_gamess_qcxms.py
@@ -43,7 +43,7 @@ def read_file(file):
     return lines
 
 
-def read_parameters(param_file):   
+def read_parameters_pbs(param_file):   
     mylist = [] 
     lines = read_file(param_file)
     for line in lines:
@@ -60,24 +60,47 @@ def read_parameters(param_file):
     return mylist
 
 
-def write_pbs_from_template(mylist, molname, templatename, file):
-    content = templatename.render(MOLNAME=molname, WALLTIME=mylist[0],
+def load_template(dir, filename):
+    template_path = Path(dir).resolve()
+    file_loader = FileSystemLoader(template_path)
+    environment = Environment(loader=file_loader)
+    load_template = environment.get_template(filename)
+    return load_template
+
+
+def write_pbs_from_template(mylist, molname, template_name, file):
+    content = template_name.render(MOLNAME=molname, WALLTIME=mylist[0],
                                   NCPUS=mylist[1], MEM=mylist[2], 
                                   SCRATCH_LOCAL=mylist[3], USER_EMAIL=mylist[4])
     with open(file, 'w') as message:
         message.write(content)
 
 
+def write_gamess_input(multiplicity, mol, mol_input_path):
+    name = mol.GetProp("NAME")
+    AllChem.EmbedMolecule(mol, maxAttempts=10000, useRandomCoords=False)
+    conf = mol.GetConformer()
+    opt = f""" $CONTRL SCFTYP={get_method(multiplicity)} MULT={multiplicity} NPRINT=-5 RUNTYP=OPTIMIZE $END\n $STATPT OPTTOL=0.0005 NSTEP=100 NPRT=-2 $END\n $BASIS GBASIS=N31 NGAUSS=6 $END """
+    with open(mol_input_path, 'w') as outfile:
+        outfile.write(f"{opt}{os.linesep}")
+        outfile.write(f"{os.linesep}")
+        outfile.write(f" $DATA{os.linesep}")
+        outfile.write(f"{name}{os.linesep}")
+        outfile.write(f"C1{os.linesep}")
+        for (i, a) in enumerate(mol.GetAtoms()):
+            pos = conf.GetAtomPosition(i)
+            outfile.write(f"{a.GetSymbol()}\t{a.GetAtomicNum()}{pos.x:>16.10f}{pos.y:>16.10f}{pos.z:>16.10f}{os.linesep}")
+        outfile.write(f"$END{os.linesep}")
+
+
 listarg = argparse.ArgumentParser()
 listarg.add_argument('--sdf_input', type=str) 
 listarg.add_argument('--params_input', type=str) 
+listarg.add_argument('--project_name', type=str) 
+
 args = listarg.parse_args()
 
 if __name__ == "__main__":
-
-    template_path = Path("templates").resolve()
-    file_loader = FileSystemLoader(template_path)
-    environment = Environment(loader=file_loader)
 
     molfile = Chem.SDMolSupplier(args.sdf_input)
 
@@ -85,40 +108,23 @@ if __name__ == "__main__":
         mol = Chem.AddHs(mol)
         chem_class = mol.GetProp("Class").replace(" ", "_")
         inchikey = mol.GetProp("InChIKey")
-        name = mol.GetProp("NAME")
+        
+        projectdir = Path.cwd() / args.project_name
+        Path.mkdir(projectdir, parents=True, exist_ok=True)
 
-        moldir = Path.cwd() / "classes" / chem_class / inchikey / "Optimization"
+        moldir = Path.cwd() / args.project_name / "classes" / chem_class / inchikey / "Optimization"
         Path.mkdir(moldir, parents=True, exist_ok=True)
         mol_input_path = moldir / (inchikey + ".inp")
         Path.touch(mol_input_path)
 
-        spectradir = Path.cwd() / "classes" / chem_class / inchikey / "Spectra"
+        spectradir = Path.cwd() / args.project_name / "classes" / chem_class / inchikey / "Spectra"
         Path.mkdir(spectradir, parents=True, exist_ok=True)
         spectrum_input_path = spectradir / ("qcxms" + ".in")
         Path.touch(spectrum_input_path)
-
-        AllChem.EmbedMolecule(mol, maxAttempts=10000, useRandomCoords=False)
-        conf = mol.GetConformer()
+        
         multiplicity = CalculateSpinMultiplicity(mol)
-        opt = f""" $CONTRL SCFTYP={get_method(multiplicity)} \
-            MULT={multiplicity} NPRINT=-5 RUNTYP=OPTIMIZE $END\n \
-            $STATPT OPTTOL=0.0005 NSTEP=100 NPRT=-2 $END\n $BASIS \
-            GBASIS=N31 NGAUSS=6 $END """
+        write_gamess_input(multiplicity, mol, mol_input_path)
 
-        with open(mol_input_path, 'w') as outfile:
-            outfile.write(f"{opt}{os.linesep}")
-            outfile.write(f"{os.linesep}")
-            outfile.write(f" $DATA{os.linesep}")
-            outfile.write(f"{name}{os.linesep}")
-            outfile.write(f"C1{os.linesep}")
-            
-            for (i, a) in enumerate(mol.GetAtoms()):
-                pos = conf.GetAtomPosition(i)
-                outfile.write(f"{a.GetSymbol()}\t{a.GetAtomicNum()}{pos.x:>16.10f}{pos.y:>16.10f}{pos.z:>16.10f}{os.linesep}")
-            
-            outfile.write(f"$END{os.linesep}")
-
-        pbs_template = environment.get_template("optimization_gamess_template.pbs")
         mol_pbs_path = moldir / (inchikey + ".pbs")
-
-        write_pbs_from_template(read_parameters(args.params_input), inchikey, pbs_template, mol_pbs_path)
+        pbs_template = load_template("templates", "optimization_gamess_template.pbs")
+        write_pbs_from_template(read_parameters_pbs(args.params_input), inchikey, pbs_template, mol_pbs_path)
