dae1e51db10da028bffc16da1192f9d8dbfe03bc
wverastegui <wudmir.rojas@recetox.muni.cz>
2023-05-15 10:50:03 +0200

    draft full computation of spectra script

--- codes/array_sub.sh
+++ codes/array_sub.sh
@@ -25,17 +25,14 @@ for dir in classes/*/*/*; do
         echo 'submitted neutral MD job'
         job1=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
 
+        echo 'submitted prep production MD job'
+        job2=$(qsub -W depend=afterok:$job1 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/prep_prod_run_ei_md.pbs)
+
+        echo 'submitted production run EI MD job'
+        job3=$(qsub -W depend=afterok:$job2 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" -J 1-$n_traj $bin/prod_run_ei_md.pbs)
+
       fi
     fi
     cd $work_dir
 done
 
-
-# echo 'submitted neutral MD job'
-# job1=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
-
-# echo 'submitted prep production MD job'
-# job2=$(qsub -W depend=afterok:$job1 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/prep_prod_run_ei_md.pbs)
-
-# echo 'submitted production run EI MD job'
-# job3=$(qsub -W depend=afterok:$job2 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" -J 1-$n_traj $bin/prod_run_ei_md.pbs)
