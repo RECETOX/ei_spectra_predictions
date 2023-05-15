d451c26db0b94458a11a36d948f16b7f75ccedb6
wverastegui <wudmir.rojas@recetox.muni.cz>
2023-05-15 09:45:21 +0200

    Draft of batch jobs submission script

--- codes/array_sub.sh
+++ codes/array_sub.sh
@@ -1,25 +1,41 @@
 #!/bin/sh
-file_xyz=`basename $(ls *.xyz)`
-file_in=`basename $(ls *.in)`
 
-keyword_ntraj=`head -n 1 $file_in | awk '{ print $1 }'` 
-
-if [[ "$keyword_ntraj" ]]; then
-    n_traj=`head -n 1 $file_in | awk '{ print $2 }'`
-else 
-    n_traj=$((`head -n 1 $file_xyz`*25))
+if [ -t 0 ]
+then
+  walltime=$(grep 'WALLTIME' $data $1 | awk '{ print $3 }')
+  user_email=$(grep 'USER_EMAIL' $data $1 | awk '{ print $3 }')
+  bin=$(grep 'BIN' $data $1 | awk '{ print $3 }')
 fi
 
-molname=`basename $file_xyz .xyz`
-walltime=02:00:00
-bin=/storage/brno2/home/wrojasv/MassSpec/QC_Spectra_Predictions/local_run_test/bin_test
-user_email=wrojasv@recetox.muni.cz
+work_dir=$(pwd)
+for dir in classes/*/*/*; do
+   basedir=`basename $dir`
+   molname=`dirname $dir | xargs basename`
+   if [[ $basedir == "Spectra" ]]; then
+      cd $work_dir/$dir
+      file_xyz=`ls $molname.xyz`
+      file_in=`ls qcxms.in`
+      if [ -f "$file_xyz" ]; then
+        keyword_ntraj=`head -n 1 $file_in | awk '{ print $1 }'` 
+        if [[ "$keyword_ntraj" ]]; then
+          n_traj=`head -n 1 $file_in | awk '{ print $2 }'`
+        else 
+          n_traj=$((`head -n 1 $file_xyz`*25))
+        fi
+        echo 'submitted neutral MD job'
+        job1=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
 
-echo 'submitted neutral MD job'
-job1=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
+      fi
+    fi
+    cd $work_dir
+done
 
-echo 'submitted prep production MD job'
-job2=$(qsub -W depend=afterok:$job1 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/prep_prod_run_ei_md.pbs)
 
-echo 'submitted production run EI MD job'
-job3=$(qsub -W depend=afterok:$job2 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" -J 1-$n_traj $bin/prod_run_ei_md.pbs)
+# echo 'submitted neutral MD job'
+# job1=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
+
+# echo 'submitted prep production MD job'
+# job2=$(qsub -W depend=afterok:$job1 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/prep_prod_run_ei_md.pbs)
+
+# echo 'submitted production run EI MD job'
+# job3=$(qsub -W depend=afterok:$job2 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" -J 1-$n_traj $bin/prod_run_ei_md.pbs)
