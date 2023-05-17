514603780ec0c278613b527eeb7638c48634fb35
wverastegui <wudmir.rojas@recetox.muni.cz>
2023-05-17 15:50:40 +0200

    renamed array_sub.sh to submit_qcxms_batch_jobs.sh

---
+++ codes/submit_qcxms_batch_jobs.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ -t 0 ]
+then
+  walltime=$(grep 'WALLTIME' $data $1 | awk '{ print $3 }')
+  user_email=$(grep 'USER_EMAIL' $data $1 | awk '{ print $3 }')
+  bin=$(grep 'BIN' $data $1 | awk '{ print $3 }')
+fi
+
+work_dir=$(pwd)
+for dir in classes/*/*/*; do
+   basedir=`basename $dir`
+   molname=`dirname $dir | xargs basename`
+   if [[ $basedir == "Spectra" ]]; then
+      cd $work_dir/$dir
+      file_xyz=`ls $molname.xyz`
+      file_in=`ls qcxms.in`
+      if [ -f "$file_xyz" ]; then
+        keyword_ntraj=`head -n 1 $file_in | awk '{ print $1 }'` 
+        if [[ "$keyword_ntraj" ]]; then
+          n_traj=`head -n 1 $file_in | awk '{ print $2 }'`
+        else 
+          n_traj=$((`head -n 1 $file_xyz`*25))
+        fi
+
+        if [ -f "qcxms.gs" ]; then
+            echo "qcxms.gs already exists: "$molname
+        else
+            echo "submitted neutral MD job: "$molname
+            job1=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
+        fi
+
+        if [ -d "TMPQCXMS" ];then
+            echo "TMPQCXMS directory already exists: "$molname
+        else
+            echo "submitted prep production MD job: "$molname
+            job2=$(qsub -W depend=afterok:$job1 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/prep_prod_run_ei_md.pbs)
+        fi
+
+        if [ -d "TMPQCXMS" ]; then
+            echo "Succesfull EI-MD exists: "$molname
+        else
+            echo "submitted production run EI MD job: "$molname
+            job3=$(qsub -W depend=afterok:$job2 -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" -J 1-$n_traj $bin/prod_run_ei_md.pbs)
+        fi
+      fi
+    fi
+    cd $work_dir
+done
+
