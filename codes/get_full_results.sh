bfb03aae1b5837c279b0d6522c862f8df64653a8
wverastegui <wudmir.rojas@recetox.muni.cz>
2023-05-25 09:42:04 +0200

    added script to collect results

--- codes/get_full_results.sh
+++ codes/get_full_results.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 if [ -t 0 ]
 then
   bin=$(grep 'BIN' $data $1 | awk '{ print $3 }')
@@ -10,13 +11,16 @@ if [ ! -d "results" ]; then
   mkdir $work_dir/results
 fi
 
+rm -f $work_dir/results/all_results.msp
+
 for dir in classes/*/*/*; do
   basedir=`basename $dir`
-  molname=`dirname $dir | xargs basename`
+  inchikey=`dirname $dir | xargs basename`
   if [[ $basedir == "Spectra" ]]; then
     cd $work_dir/$dir
 
     if [ -d "TMPQCXMS" ]; then
+
       rm -f $work_dir/$dir/tmpqcxms.res
       cd $work_dir/$dir/TMPQCXMS
       for i in TMP.*; do
@@ -29,25 +33,21 @@ for dir in classes/*/*/*; do
       cd $work_dir/$dir
 
       if [ ! -f "result.jdx" ]; then
-        echo "$bin/plotms -f tmpqcxms.res"
         $bin/plotms -f tmpqcxms.res 
       fi
-      rm -f $work_dir/$dir/result.csv
+      rm -f result.csv
     
-      if [ -f "result.jdx" ]; then
-        kword=$(grep 'NPOINTS' result.jdx)
-        num_peaks=$(sed 's/^[^=]*=//' <<< "$kword")
-        sed -n '/PEAK/,/END/{/PEAK/!{/END/!p}}' result.jdx > temp.dat
-        awk '{print $1, $2}' temp.dat > tempa.dat
-        sed '1s/^/Name: '$molname'\nNum Peaks: '$num_peaks'\n/' tempa.dat > $work_dir/results/$(sed -e 's/.[^.]*$//' <<< "$molname.jdx")'.msp'
-        rm temp.dat tempa.dat
-      fi
+      molname=`sed -n '2{p;q}'  $inchikey.xyz`
+      kword=$(grep 'NPOINTS' result.jdx)
+      num_peaks=$(sed 's/^[^=]*=//' <<< "$kword")
+      sed -n '/PEAK/,/END/{/PEAK/!{/END/!p}}' result.jdx > temp.dat
+      awk '{print $1, $2}' temp.dat > tempa.dat
+      sed "1s/^/NAME: $molname\nINCHIKEY: $inchikey\nNum Peaks: $num_peaks\n/" tempa.dat >> $work_dir/results/all_results.msp
+      sed -i '$a\ ' $work_dir/results/all_results.msp
+      rm temp.dat tempa.dat 
 
     fi
   fi
     cd $work_dir
 done
 
-if [ ! -f "all_results.msp" ]; then
-  cat $work_dir/results/*.msp >> $work_dir/results/all_results.msp
-fi
