e790e38ab1122bdb4c835b1e693e527713902c49
wverastegui <wudmir.rojas@recetox.muni.cz>
2023-09-06 11:33:20 +0200

    Updated format

--- codes/submit_qcxms_batch_jobs.sh
+++ codes/submit_qcxms_batch_jobs.sh
@@ -31,32 +31,33 @@ for dir in classes/*/*/*; do
       if [ -f "missing_trajs.res" ]; then  
         readarray -t traj_array <  missing_trajs.res
         ended_job=true
+        echo "submitted missing production run EI MD job : "$class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
         for i in ${traj_array[@]#*.}; do
           prod_job=$(qsub -N $molname -M $user_email -l walltime=$walltime -l select=1:ncpus=1:mem=$MEM:scratch_local=100gb -v "bin=$bin" -J $i-$((i+1)):2 $bin/prod_run_ei_md.pbs)
         done
 
       elif [ -f "tmpqcxms.res" ]; then
-        echo "successful SPECTRUM simulated in class: " $class_name": molname" $molname >> $work_dir/info_submit_batch_jobs.log
+        echo "successful SPECTRUM simulated in class : " $class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
         continue
       else
         ended_job=true
-        echo "submitted neutral MD job: "$class_name": molname" $molname >> $work_dir/info_submit_batch_jobs.log
+        echo "submitted neutral MD job : "$class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
         neutral_job=$(qsub -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/neutral_run_md.pbs)
 
-        echo "submitted prep production MD job: "$class_name": molname" $molname >> $work_dir/info_submit_batch_jobs.log
+        echo "submitted pre-production MD job : "$class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
         prep_job=$(qsub -W depend=afterok:$neutral_job -N $molname -M $user_email -l walltime=$walltime -v "bin=$bin" $bin/prep_prod_run_ei_md.pbs)
         
-        echo "submitted production run EI MD job: "$class_name": molname" $molname >> $work_dir/info_submit_batch_jobs.log
+        echo "submitted production run EI MD job : "$class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
         prod_job=$(qsub -W depend=afterok:$prep_job -N $molname -M $user_email -l walltime=$walltime -l select=1:ncpus=1:mem=$MEM:scratch_local=100gb -v "bin=$bin" -J 1-$n_traj $bin/prod_run_ei_md.pbs)
       fi
 
       if [ "$ended_job"=true ];then
-        echo "get tmpqcxms.res: "$class_name": molname" $molname >> $work_dir/info_submit_batch_jobs.log
+        echo "get tmpqcxms.res : "$class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
         getres_job=$(qsub -W depend=afterok:$prod_job -N $molname -M $user_email -l walltime=$walltime  -v "threshold_traj=$threshold_traj, n_traj=$n_traj" $bin/getres.pbs)
       fi
 
     else
-      echo "NOT found xyz in class" $class_name": molname" $molname >> $work_dir/info_submit_batch_jobs.log
+      echo "NOT found xyz in class :" $class_name" --> molname :" $molname >> $work_dir/info_submit_batch_jobs.log
     fi
   fi
   cd $work_dir
