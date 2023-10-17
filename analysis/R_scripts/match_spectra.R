bd27256b753e6dc358287f6a645b8f0e82a5120e
wverastegui <wudmir@gmail.com>
2023-10-17 16:31:43 +0200

    refactored

--- analysis/R_scripts/match_spectra.R
+++ analysis/R_scripts/match_spectra.R
@@ -3,57 +3,34 @@ library(MetaboAnnotation)
 library(Spectra)
 library(MsBackendMsp)
 library(dplyr)
-library(pheatmap)
-library(ggplot2)
 
+match_spectra <- function(reference_file, simulated_file, output_file, ppm = 5, requirePrecursor = FALSE, FUN = MsCoreUtils::ndotproduct, THRESHFUN = function(x) which(x >= 0.0001), THRESHFUN_REVERSE = function(x) which(x >= 0.0001)) {
+  # Read the reference and simulated spectra from MSP files
+  data_reference <- Spectra(reference_file, source = MsBackendMsp::MsBackendMsp())
+  data_simulated <- Spectra(simulated_file, source = MsBackendMsp::MsBackendMsp())
 
-# Define file paths
-reference_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/data/RECETOX_GC-EI_MS_20201028.msp'
-simulated_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/data/matchms_filtering_default_filter_0.01_int_70_700_mz.msp'
-output_file <- "/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/R_scripts/output/matchSpectra_results_source_0.01_int.tsv"
+  # Define match parameters
+  match_param <- MatchForwardReverseParam(requirePrecursor = requirePrecursor,
+                                          ppm = ppm, 
+                                          FUN = FUN, 
+                                          THRESHFUN = THRESHFUN, 
+                                          THRESHFUN_REVERSE = THRESHFUN_REVERSE)
 
-# Read the reference and simulated spectra from MSP files
-data_reference <- Spectra(reference_file, source = MsBackendMsp::MsBackendMsp())
-data_simulated <- Spectra(simulated_file, source = MsBackendMsp::MsBackendMsp())
+  # Perform matching
+  matched_spectra <- matchSpectra(data_simulated, data_reference, match_param)
 
-
-#################################
-# matchSpectra
-#################################
-match_param <- MatchForwardReverseParam(requirePrecursor = FALSE,
-    ppm = 5, 
-    FUN = MsCoreUtils::ndotproduct, 
-    THRESHFUN = function(x) which(x >= 0.0001), 
-    THRESHFUN_REVERSE = function(x) which(x >= 0.0001) 
-)
-
-matched_spectra <- matchSpectra(data_simulated, data_reference, match_param)
-
-print(matched_spectra)
-
-matched_spectra_df <- spectraData(matched_spectra, columns = c("name", "target_name", "reverse_score", "score", "presence_ratio", "matched_peaks_count"))
-
-matched_spectra_df <- as.data.frame(matched_spectra_df)
-
-matched_spectra_df <- matched_spectra_df %>%
+  # Convert matched spectra to data frame
+  matched_spectra_df <- spectraData(matched_spectra, columns = c("name", "target_name", "reverse_score", "score", "presence_ratio", "matched_peaks_count"))
+  matched_spectra_df <- as.data.frame(matched_spectra_df)
+  matched_spectra_df <- matched_spectra_df %>%
     filter_all(all_vars(!is.na(.)))
 
-write.table(matched_spectra_df, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE)
+  # Write results to output file
+  write.table(matched_spectra_df, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE)
+}
 
-#################################
-# compareSpectra
-#################################
+reference_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/data/RECETOX_GC-EI_MS_20201028_norm_matchms.msp'
+query_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data/filtered/matchms_filtering_default_filter_1%_all_peaks.msp'
+output_file <- "/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data/matching/matchspectra_R/match_1%/matchSpectra_results_source_1%_all_peaks.tsv"
 
-# comp_spectra <- compareSpectra(data_simulated, data_reference, fun = "dotproduct", ppm = 5)
-
-# output_file <- "/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/R_scripts/output/comp_spectra.tsv"
-
-# write.table(comp_spectra, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE)
-
-# pheatmap_obj <- pheatmap(comp_spectra, cluster_rows = FALSE, cluster_cols = FALSE)
-
-# ggsave("/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/R_scripts/output/pheatmap.png", plot = pheatmap_obj, device = "png")
-
-
-
-
+match_spectra(reference_file, query_file, output_file)
