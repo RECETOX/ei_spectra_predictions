fe98b92eab798e467eb28a9ab8b8ba308ba6b80a
wverastegui <wudmir@gmail.com>
2023-10-18 13:22:54 +0200

    updated

--- analysis/R_scripts/match_spectra.R
+++ analysis/R_scripts/match_spectra.R
@@ -10,14 +10,14 @@ match_spectra <- function(reference_file, simulated_file, output_file, ppm = 5, 
   data_simulated <- Spectra(simulated_file, source = MsBackendMsp::MsBackendMsp())
 
   # Define match parameters
-  match_param <- MatchForwardReverseParam(requirePrecursor = requirePrecursor,
+  match_param <- MetaboAnnotation::MatchForwardReverseParam(requirePrecursor = requirePrecursor,
                                           ppm = ppm, 
                                           FUN = FUN, 
                                           THRESHFUN = THRESHFUN, 
                                           THRESHFUN_REVERSE = THRESHFUN_REVERSE)
 
   # Perform matching
-  matched_spectra <- matchSpectra(data_simulated, data_reference, match_param)
+  matched_spectra <- MetaboAnnotation::matchSpectra(data_simulated, data_reference, match_param)
 
   # Convert matched spectra to data frame
   matched_spectra_df <- spectraData(matched_spectra, columns = c("name", "target_name", "reverse_score", "score", "presence_ratio", "matched_peaks_count"))
@@ -29,8 +29,21 @@ match_spectra <- function(reference_file, simulated_file, output_file, ppm = 5, 
   write.table(matched_spectra_df, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE)
 }
 
-reference_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/data/RECETOX_GC-EI_MS_20201028_norm_matchms.msp'
-query_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data/filtered/matchms_filtering_default_filter_1%_all_peaks.msp'
-output_file <- "/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data/matching/matchspectra_R/match_1%/matchSpectra_results_source_1%_all_peaks.tsv"
+base_path <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data'
+# reference_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/data/RECETOX_GC-EI_MS_20201028_norm_matchms.msp'
+# query_file <- '/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data/filtered/matchms_filtering_default_filter_1%_all_peaks.msp'
+# output_file <- "/home/wrojas/dev/RECETOX/ei_spectra_predictions/analysis/data/matching/matchspectra_R/match_1%/matchSpectra_results_source_1%_all_peaks.tsv"
 
-match_spectra(reference_file, query_file, output_file)
+
+# match_spectra(reference_file, query_file, output_file)
+match_spectra(
+  file.path(base_path, "experimental/RECETOX_GC-EI_MS_20201028_norm_matchms.msp"),
+  file.path(base_path, 'filtered/matchms_filtering_default_filter_1%_all_peaks.msp'),
+  file.path(base_path, 'outputs/matchspectra_R/match_1%/matchSpectra_results_source_1%_all_peaks_norm.tsv')
+)
+
+match_spectra(
+  file.path(base_path, "experimental/RECETOX_GC-EI_MS_20201028.msp"),
+  file.path(base_path, 'filtered/matchms_filtering_default_filter_1%_all_peaks.msp'),
+  file.path(base_path, 'outputs/matchspectra_R/match_1%/matchSpectra_results_source_1%_all_peaks.tsv')
+)
