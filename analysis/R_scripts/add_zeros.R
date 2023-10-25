c6866a1eaf37bf6da7106b46b41cb8b8ecaccc8a
wverastegui <wudmir@gmail.com>
2023-10-25 15:47:54 +0200

    add zeros

--- analysis/R_scripts/add_zeros.R
+++ analysis/R_scripts/add_zeros.R
@@ -4,39 +4,61 @@ library(Spectra)
 library(MsBackendMsp)
 library(dplyr)
 
+
+add_zeros <- function(out_file, match_file, reference_file, simulated_file, query, reference, scores, matches) {
+  # if (any(is.na(match_file$query))) {
+  #   print("There are NA values in the 'query' column.")
+  # }
+
+  # # Check for NA values in the 'reference' column
+  # if (any(is.na(match_file$reference))) {
+  #   print("There are NA values in the 'reference' column.")
+  # }
+
+  # Read the reference and simulated spectra from MSP files
+  data_reference <- Spectra(reference_file, source = MsBackendMsp::MsBackendMsp())
+  data_simulated <- Spectra(simulated_file, source = MsBackendMsp::MsBackendMsp())
+
+  sim_names <- data_simulated$name
+  ref_names <- data_reference$name
+  #match_file[is.na(match_file[[3]]), 3] <- 0
+
+  name_combination <- expand.grid(sim_names, ref_names)
+  zero_scores <- data.frame(matrix(ncol = ncol(match_file), nrow = 0))
+  names(zero_scores) <- names(match_file)
+
+  for (i in 1:nrow(name_combination)) {
+    matches <- match_file[match_file[query] == as.character(name_combination$Var1[i]) & match_file[reference] == as.character(name_combination$Var2[i]),]
+    if(nrow(matches) == 0) {
+      if(length(names(match_file)) == 6) {
+        new_row <- data.frame(name_combination$Var1[i], name_combination$Var2[i], 0, 0, 0, 0)
+        names(new_row) <- names(match_file)
+        zero_scores <- rbind(zero_scores, new_row)
+      }  else {
+        new_row <- data.frame(name_combination$Var1[i], name_combination$Var2[i], 0, 0)
+        names(new_row) <- names(match_file)
+        zero_scores <- rbind(zero_scores, new_row)
+      }
+    }
+  }
+
+  names(zero_scores) <- c(query, reference, scores, matches)
+  match_file <- rbind(match_file, zero_scores)
+  write.table(match_file, file = out_file)
+}
+
+
 reference_file <- file.path('analysis/data/experimental/RECETOX_GC-EI_MS_20201028_norm_matchms.msp')
 simulated_file <- file.path('analysis/data/filtered/matchms_filtering_default_filter_1%_all_peaks.msp')
-matchms_match <- read.table(file.path('analysis/data/matchms_top5_comparison.tsv'), header = TRUE, sep = "\t", fill = TRUE)
 
-# Check for NA values in the 'query' column
-if (any(is.na(matchms_match$query))) {
-  print("There are NA values in the 'query' column.")
-}
+# for MatchMS
+# matchms_match <- read.table(file.path('analysis/data/matchms_top5_comparison.tsv'), header = TRUE, sep = "\t", fill = TRUE)
+# out_file_matchms = file.path('analysis/R_scripts/matchms_top5_comparison_with_zeros.tsv')
+# add_zeros(out_file_matchms, as.data.frame(matchms_match), reference_file, simulated_file, 
+#  "query", "reference", "CosineHungarian_0.01_0.0_1.0_scores", "CosineHungarian_0.01_0.0_1.0_matches")
 
-# Check for NA values in the 'reference' column
-if (any(is.na(matchms_match$reference))) {
-  print("There are NA values in the 'reference' column.")
-}
-
-# Read the reference and simulated spectra from MSP files
-data_reference <- Spectra(reference_file, source = MsBackendMsp::MsBackendMsp())
-data_simulated <- Spectra(simulated_file, source = MsBackendMsp::MsBackendMsp())
-
-sim_names <- data_simulated$name
-ref_names <- data_reference$name
-
-name_combination <- expand.grid(sim_names, ref_names)
-zero_scores <- data.frame(query = character(), reference = character(), CosineHungarian_0.01_0.0_1.0_scores = numeric(), CosineHungarian_0.01_0.0_1.0_matches = numeric())
-
-for (i in 1:nrow(name_combination)) {
-  matches <- matchms_match[matchms_match$query == name_combination$Var1[i] & matchms_match$reference == name_combination$Var2[i],]
-  if(nrow(matches) == 0) {
-    new_row <- data.frame(name_combination$Var1[i], name_combination$Var2[i], 0, 0)
-    names(new_row) <- c("query", "reference", "CosineHungarian_0.01_0.0_1.0_scores", "CosineHungarian_0.01_0.0_1.0_matches")
-    zero_scores <- rbind(zero_scores, new_row)
-  }
-}
-
-names(zero_scores) <- c("query", "reference", "CosineHungarian_0.01_0.0_1.0_scores", "CosineHungarian_0.01_0.0_1.0_matches")
-matchms_match <- rbind(matchms_match, zero_scores)
-write.table(matchms_match, file = "matchms_top5_comparison_with_zeros.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+# for MatchSpectra
+mspectra_match <- read.table(file.path('analysis/data/outputs/matchspectra_R/metaboannotation_top5.tsv'), header = TRUE, sep = "\t", fill = TRUE)
+out_file_mspectra = file.path('analysis/R_scripts/metaboannotation_top5_with_zeros.tsv')
+add_zeros(out_file_mspectra, as.data.frame(mspectra_match), reference_file, simulated_file,
+ "name", "target_name", "score", "matched_peaks_count")
--- analysis/R_scripts/match_spectra.R
+++ analysis/R_scripts/match_spectra.R
@@ -25,34 +25,36 @@ match_spectra <- function(reference_file, simulated_file, output_file, ppm = 5) 
   # Convert matched spectra to data frame
   matched_spectra_df <- spectraData(matched_spectra, columns = c("name", "target_name", "reverse_score", "score", "presence_ratio", "matched_peaks_count"))
   matched_spectra_df <- as.data.frame(matched_spectra_df)
- # matched_spectra_df <- matched_spectra_df # %>%    filter_all(all_vars(!is.na(.)))
 
-  # matched_spectra_filtered <- subset(matched_spectra_df, "name" == "target_name")
-  # missing_names <- setdiff(data_simulated$name, matched_spectra_filtered$name)
-  # print(missing_names)
-  # file_path <- "missing_names_nofilter_NA.txt"
-  # write.table(missing_names, file_path, col.names = FALSE, row.names = FALSE, quote = FALSE)
- 
   # Write results to output file
+  matched_spectra_df[is.na(matched_spectra_df[[3]]), 3] <- 0
+
   write.table(matched_spectra_df, file = output_file, sep = "\t", quote = FALSE, row.names = FALSE)
 }
 
 base_path <- 'analysis/data'
 
-match_spectra(
-  file.path(base_path, "experimental/RECETOX_GC-EI_MS_20201028_norm_matchms.msp"),
-  file.path(base_path, 'filtered/matchms_filtering_default_filter_1%_all_peaks.msp'),
-  file.path(base_path, 'outputs/matchspectra_R/match_1%/metaboannotation_all_peaks_zeros_temp.tsv')
-)
-
-reference_file <- file.path(base_path, "experimental/problematic_spectra.msp")
-simulated_file <- file.path(base_path, 'filtered/problematic_predicted.msp')
-
+reference_file <- file.path(base_path, 'experimental/RECETOX_GC-EI_MS_20201028_matchms_filter_top5.msp')
+simulated_file <- file.path(base_path, 'filtered/1%/Galaxy29-[matchms_filtering_on_data_27]_1%_tp5.msp')
 match_spectra(
   reference_file,
-  query_file,
-  file.path(base_path, 'outputs/matchspectra_R/problematic_scores.tsv')
+  simulated_file,
+  file.path(base_path, 'outputs/matchspectra_R/metaboannotation_top5.tsv')
 )
+
+# match_spectra(
+#   file.path(base_path, "experimental/RECETOX_GC-EI_MS_20201028_norm_matchms.msp"),
+#   file.path(base_path, 'filtered/matchms_filtering_default_filter_1%_all_peaks.msp'),
+#   file.path(base_path, 'outputs/matchspectra_R/match_1%/metaboannotation_all_peaks_zeros_temp.tsv')
+# )
+
+# reference_file <- file.path(base_path, "experimental/problematic_spectra.msp")
+# simulated_file <- file.path(base_path, 'filtered/problematic_predicted.msp')
+# match_spectra(
+#   reference_file,
+#   query_file,
+#   file.path(base_path, 'outputs/matchspectra_R/problematic_scores.tsv')
+# )
 # match_spectra(
 #   file.path(base_path, "experimental/RECETOX_GC-EI_MS_20201028.msp"),
 #   file.path(base_path, 'filtered/matchms_filtering_default_filter_1%_all_peaks.msp'),
