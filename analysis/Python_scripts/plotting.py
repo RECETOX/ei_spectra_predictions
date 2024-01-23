6a95de4e18e6225cc531faa78266a2ba3f6611e5
hechth <helge.hecht@recetox.muni.cz>
2024-01-23 10:24:03 +0100

    updated plots and added code to make plots absed on properties

--- analysis/Python_scripts/plotting.py
+++ analysis/Python_scripts/plotting.py
@@ -5,8 +5,9 @@ import seaborn as sns
 import plotly.graph_objs as go
 import numpy as np
 
+
 def make_boxplot(grouped_df: pd.DataFrame, colname: str, legend: str):
-    #Create the plot with a width of 10 inches
+    # Create the plot with a width of 10 inches
     fig, ax = plt.subplots(figsize=(17, 5))
 
     # Create a list of DataFrames, one for each group
@@ -14,19 +15,18 @@ def make_boxplot(grouped_df: pd.DataFrame, colname: str, legend: str):
 
     # Create a boxplot for each DataFrame
     ax.boxplot([df[colname] for df in grouped_dfs])
-    
 
     labels = [f"{group} ({len(x)})" for group, x in grouped_df]
     # Set the x-axis tick labels and increase the distance between ticks to 0.5 inches
     ax.set_xticklabels(labels, rotation=90)
     ax.tick_params(axis='x', which='major', pad=0.5)
-    
+
     # Set the x-axis label, y-axis label, and plot title
-    #ax.set_ylim([0, 1])
+    # ax.set_ylim([0, 1])
     ax.set_xlabel('Class', fontsize=16)
     ax.set_ylabel(colname, fontsize=16)
     ax.legend([legend], fontsize=14)
-    
+
     # Show the plot
     plt.show()
 
@@ -43,38 +43,48 @@ def plot_histograms_sidebyside(same_query_ref1, same_query_ref2, same_query_ref3
     x5 = same_query_ref5[column_name]
 
     # Create histogram traces
-    trace1 = go.Histogram(x=x1, nbinsx=n_bins, name='All peaks ({})'.format(len(x1)), xbins=dict(start=0, end=1))
-    trace2 = go.Histogram(x=x2, nbinsx=n_bins, name='Top 3 peaks ({})'.format(len(x2)), xbins=dict(start=0, end=1))
-    trace3 = go.Histogram(x=x3, nbinsx=n_bins, name='Top 5 peaks ({})'.format(len(x3)), xbins=dict(start=0, end=1))
-    trace4 = go.Histogram(x=x4, nbinsx=n_bins, name='Top 10 peaks ({})'.format(len(x4)), xbins=dict(start=0, end=1))
-    trace5 = go.Histogram(x=x5, nbinsx=n_bins, name='Top 20 peaks ({})'.format(len(x5)), xbins=dict(start=0, end=1))
+    trace1 = go.Histogram(x=x1, nbinsx=n_bins, name='All peaks ({})'.format(
+        len(x1)), xbins=dict(start=0, end=1))
+    trace2 = go.Histogram(x=x2, nbinsx=n_bins, name='Top 3 peaks ({})'.format(
+        len(x2)), xbins=dict(start=0, end=1))
+    trace3 = go.Histogram(x=x3, nbinsx=n_bins, name='Top 5 peaks ({})'.format(
+        len(x3)), xbins=dict(start=0, end=1))
+    trace4 = go.Histogram(x=x4, nbinsx=n_bins, name='Top 10 peaks ({})'.format(
+        len(x4)), xbins=dict(start=0, end=1))
+    trace5 = go.Histogram(x=x5, nbinsx=n_bins, name='Top 20 peaks ({})'.format(
+        len(x5)), xbins=dict(start=0, end=1))
 
     # Create layout
     layout = go.Layout(title=title,
-                       xaxis=dict(title=xaxis_title, range=[0, 1], dtick=0.1, tickfont=dict(size=15)),
-                       yaxis=dict(title='Frequency', range=[0, 140], tickfont=dict(size=15)),
+                       xaxis=dict(title=xaxis_title, range=[
+                                  0, 1], dtick=0.1, tickfont=dict(size=15)),
+                       yaxis=dict(title='Frequency', range=[
+                                  0, 140], tickfont=dict(size=15)),
                        legend=dict(x=0.81, y=1.0),
                        font=dict(size=17))
 
     # Create figure
-    fig = go.Figure(data=[trace1, trace2, trace3, trace4, trace5], layout=layout)
+    fig = go.Figure(data=[trace1, trace2, trace3,
+                    trace4, trace5], layout=layout)
 
     # Display the plot
     fig.show()
 
+
 def plot_histogram(x, xaxis_title='', title=''):
     # Define number of bins
     n_bins = 20
-  
 
     # Create histogram traces
-    trace1 = go.Histogram(x=x, nbinsx=n_bins, name='All peaks ({})'.format(len(x)), xbins=dict(start=0, end=1))
-  
+    trace1 = go.Histogram(x=x, nbinsx=n_bins, name='All peaks ({})'.format(
+        len(x)), xbins=dict(start=0, end=1))
 
     # Create layout
     layout = go.Layout(title=title,
-                       xaxis=dict(title=xaxis_title, range=[0, 1], dtick=0.1, tickfont=dict(size=15)),
-                       yaxis=dict(title='Frequency', range=[0, 140], tickfont=dict(size=15)),
+                       xaxis=dict(title=xaxis_title, range=[
+                                  0, 1], dtick=0.1, tickfont=dict(size=15)),
+                       yaxis=dict(title='Frequency', range=[
+                                  0, 140], tickfont=dict(size=15)),
                        legend=dict(x=0.81, y=1.0),
                        font=dict(size=17))
 
@@ -84,60 +94,100 @@ def plot_histogram(x, xaxis_title='', title=''):
     # Display the plot
     fig.show()
 
-def create_plot(df, path, grouping_column, xlabel):
-    sns.set_style(style='white')
-    plt.figure(figsize=(17, 5))
 
-    # Set the color palette
+def create_plot(df: pd.DataFrame,
+                grouping_column: str,
+                xlabel: str = None,
+                showlegend: bool = True,
+                normalized_matches: bool = True,
+                nist_scale: bool = True,
+                hide_labels: bool = False):
+    matches_col = 'CosineHungarian_0.01_0.0_1.0_matches'
+    scores_col = 'CosineHungarian_0.01_0.0_1.0_scores'
+
+    label_fontsize = 20
+    tick_fontsize = 13
+    text_width = 22
+
+    # Set the style
     colors = ['yellow', 'deepskyblue']
     sns.set_palette(sns.color_palette(colors))
+    sns.set_style(style='white')
 
-    ax = sns.boxplot(x=grouping_column, y="value", hue="Number", 
-                    data=df, hue_order=['CosineHungarian_0.01_0.0_1.0_matches',np.nan],
-                    medianprops={'color': 'darkgreen', 'linewidth': 4.0},
-                    flierprops={'marker': 'o', 'markersize': 10, 'markerfacecolor': 'none'})  # RUN PLOT   
+    # Calculate the number of bars and figure size
+    n_bars = df[grouping_column].nunique()
+    bar_width = 2
+    plot_width = n_bars * bar_width
+    fig = plt.figure(figsize=(plot_width, 5))
+
+
+    ax = sns.boxplot(x=grouping_column, y="value", hue="Number",
+                     data=df, hue_order=[matches_col, np.nan],
+                     medianprops={'color': 'darkgreen', 'linewidth': 4.0},
+                     flierprops={'marker': 'o', 'markersize': 10, 'markerfacecolor': 'none'})  # RUN PLOT
+    ax.legend_.remove()
+
+    if normalized_matches:
+        ax.set_ylim([0, 100])  # Set y-axis limits
+        ax.set_ylabel('ions matching reference', fontsize=label_fontsize)  # Set y-axis label
+        ax.set_yticklabels([str(f"{x}%") for x in range(0, 101, 20)])
+    else:
+        ax.set_ylabel('ion matches', fontsize=label_fontsize)
+        ax.set_ylim(0, top=df.loc[df['Number'] == matches_col, 'value'].max())  # Set y-axis limits
+
+
+    # Set font size of x-axis tick labels
+    ax.tick_params(axis='x', labelsize=tick_fontsize)
+    # Set font size of y-axis tick labels
+    ax.tick_params(axis='y', labelsize=tick_fontsize)
+
     ax2 = ax.twinx()
 
-    sns.boxplot(ax=ax2,x=grouping_column, y='value', hue='Number',
-                data=df, hue_order=[np.nan, 'CosineHungarian_0.01_0.0_1.0_scores'], 
-                medianprops={'color': 'b', 'linewidth': 4.0}, 
+    sns.boxplot(ax=ax2, x=grouping_column, y='value', hue='Number',
+                data=df, hue_order=[np.nan, scores_col],
+                medianprops={'color': 'b', 'linewidth': 4.0},
                 flierprops={'marker': 'o', 'markersize': 10, 'markerfacecolor': 'none'})
 
-    ax.legend_.remove()
-    # ax.set_ylim([0, 5])  # Set y-axis limits
-    # ax.yaxis.set_major_locator(plt.MultipleLocator(1))  # Set major tick marks
-    ax.set_ylabel('Match values', fontsize=20)  # Set y-axis label
+    ax2.legend_.remove()
+
+    if nist_scale:
+        ax2.set_ylim([0, 1000])
+    else:
+        ax2.set_ylim([0, 1])  # Set y-axis limits
+
+    ax2.set_ylabel('scores', fontsize=label_fontsize)  # Set y-axis label
+    # Set font size of y-axis tick labels
+    ax2.tick_params(axis='y', labelsize=tick_fontsize)
+
     if xlabel:
-        ax.set_xlabel(xlabel, fontsize=20)  # Set x-axis label and font size
-    ax.tick_params(axis='x', labelsize=13)  # Set font size of x-axis tick labels
-    # ax.tick_params(axis='y', labelsize=13)  # Set font size of y-axis tick labels
-    # ax.yaxis.labelpad = 10
+        # Set x-axis label and font size
+        ax.set_xlabel(xlabel, fontsize=label_fontsize)
+    else:
+        ax.set_xlabel("", fontsize=0)
+        ax2.set_xlabel("", fontsize=0)
+    
 
     # Create a count for each x-axis label
     count_data = df[grouping_column].value_counts()
 
     # # Add the count labels to the x-axis
     xlabels = [label.get_text() for label in ax.get_xticklabels()]
-    xlabels = ['\n'.join(textwrap.wrap(label + ' (' + str((count_data.loc[label] //2 )) + ')', width=25)) for label in xlabels]
+    xlabels = ['\n'.join(textwrap.wrap(
+        label + ' (' + str((count_data.loc[label] // 2)) + ')', width=text_width)) for label in xlabels]
     ax.set_xticklabels(xlabels, rotation=45, ha='right')
 
-    ax2.legend_.remove()
-    # ax2.set_ylim([0, 1])  # Set y-axis limits
-    # ax2.yaxis.set_major_locator(plt.MultipleLocator(0.2))  # Set major tick marks
-    ax2.set_ylabel('Score values', fontsize=20)  # Set y-axis label
-    ax2.tick_params(axis='y', labelsize=13)  # Set font size of y-axis tick labels
-    # ax2.yaxis.labelpad = 10
-
     # Change the legend labels
-    handles, labels = ax.get_legend_handles_labels()
-    labels[0] = 'Matches'
-    labels[1] = 'Scores'
-    ax.legend(handles, labels, loc='upper right', fontsize=14)
+    if showlegend:
+        handles, labels = ax.get_legend_handles_labels()
+        labels[0] = ax.get_ylabel()
+        labels[1] = ax2.get_ylabel()
+        ax.legend(handles, labels, loc='upper right', fontsize=14)
+    
+    if hide_labels:
+        ax.set_ylabel("", fontsize=0)
+        ax2.set_ylabel("", fontsize=0)
 
-    plt.savefig(path, dpi=300, bbox_inches='tight')  # SAVE PLOT AS PNG FILE
-    plt.show()
-    plt.clf()
-    plt.close()
+    return fig
 
 
 def scatterplot_matplotlib(df):
@@ -145,7 +195,8 @@ def scatterplot_matplotlib(df):
     scatter = plt.scatter(
         df['CosineHungarian_0.01_0.0_1.0_scores'],
         df['CosineHungarian_0.01_0.0_1.0_matches'],
-        s=df['FractionQuery'] * 200,  # Adjust the size scaling factor as needed
+        # Adjust the size scaling factor as needed
+        s=df['FractionQuery'] * 200,
         c=df['FractionReference'] * 100,
         cmap='viridis',  # change the colorscale as needed
         alpha=0.5,
@@ -153,12 +204,13 @@ def scatterplot_matplotlib(df):
         vmax=100
     )
     plt.colorbar(scatter).set_label('Reference Matched %')
-    plt.xlabel('Score')
-    plt.ylabel('Matches')
+    plt.xlabel('scores')
+    plt.ylabel('ion matches')
 
     # Add a legend for the size
     sizes = [1, 50, 100]
     for size in sizes:
-        plt.scatter([], [], c='c', alpha=0.5, s=size * 2 , label=str(size))
-    plt.legend(scatterpoints=1, title='Query Matched %', labelspacing=1, loc='upper left')
+        plt.scatter([], [], c='c', alpha=0.5, s=size * 2, label=str(size))
+    plt.legend(scatterpoints=1, title='Query Matched %',
+               labelspacing=1, loc='upper left')
     return fig
--- analysis/Python_scripts/utils.py
+++ analysis/Python_scripts/utils.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
 from rdkit import Chem
+from rdkit.Chem.AllChem import AddHs
 from itertools import combinations
 from matchms.importing import load_from_msp
+
+from matchms import set_matchms_logger_level
+set_matchms_logger_level("ERROR")
 
 def is_spectrum_for_compound(compund_name, spectrum_name):
     options = [compund_name + x for x in ["", "_isomer1", "_isomer2", " isomer 1", " isomer 2"]]
@@ -60,6 +64,9 @@ def has_organic_atoms(mol):
 
     return False
 
+def get_num_atoms_of_type(molecule, atom_type):
+    return int(len([atom for atom in molecule.GetAtoms() if atom.GetSymbol() == atom_type]))
+
 def append_classes(df, left_on):
     """
     Append molecular classes information to a DataFrame based on a specified column.
@@ -71,13 +78,14 @@ def append_classes(df, left_on):
     Returns:
     - pd.DataFrame: The input DataFrame with additional molecular classes information.
     """
-    molecules = Chem.SDMolSupplier("../../data/RECETOX_GC-EI-MS_20201028.sdf")
+    molecules = Chem.SDMolSupplier("../../data/recetox_gc-ei-ms_20201028_custom.sdf")
     class_names = pd.DataFrame({
         "class" : [m.GetProp("Class") for m in molecules],
         "superclass" : [m.GetProp("Superclass") for m in molecules],
+        "subclass" : [m.GetProp("Subclass") for m in molecules],
         "molname" : [m.GetProp("NAME") for m in molecules],
-        "n_atoms" : [m.GetNumAtoms() for m in molecules],
-        "n_bonds" : [m.GetNumBonds() for m in molecules],
+        "n_atoms" : [AddHs(m).GetNumAtoms() for m in molecules],
+        "n_bonds" : [AddHs(m).GetNumBonds() for m in molecules],
         "has_halogen": [has_halogen_atoms(m) for m in molecules],
         "smiles" : [m.GetProp("SMILES") for m in molecules],
         "Cl": [has_atom(m, 'Cl') for m in molecules],
@@ -88,6 +96,23 @@ def append_classes(df, left_on):
         "Si": [has_atom(m, 'Si') for m in molecules],
         "C,O,N,H": [has_organic_atoms(m) or has_atom(m, 'N') for m in molecules],
         "N": [has_atom(m, 'N') for m in molecules],
+        "aromatic_nitrogens": [int(m.GetProp("Aromatic Nitrogens")) for m in molecules],
+        "molecular_complexity": [float(m.GetProp("Molecular Complexity")) for m in molecules],
+        "molecular_flexibility": [float(m.GetProp("Molecular Flexibility")) for m in molecules],
+        "rotatable_bonds": [int(m.GetProp("Rotatable Bonds")) for m in molecules],
+        "amines": [int(m.GetProp("Amines")) for m in molecules],
+        "amides": [int(m.GetProp("Amides")) for m in molecules],
+        "non_ch_atoms": [int(m.GetProp("Non-C/H Atoms")) for m in molecules],
+        "stereo_centers": [int(m.GetProp("Stereo Centers")) for m in molecules],
+        "electronegative_atoms": [int(m.GetProp("Electronegative Atoms")) for m in molecules],
+        "aromatic_atoms": [int(m.GetProp("Aromatic Atoms")) for m in molecules],
+        "aromatic_rings": [int(m.GetProp("Aromatic Rings")) for m in molecules],
+        "n_chlorine": [get_num_atoms_of_type(m, "Cl") for m in molecules],
+        "n_bromine": [get_num_atoms_of_type(m, "Br") for m in molecules],
+        "n_fluorine": [get_num_atoms_of_type(m, "F") for m in molecules],
+        "n_sulfur": [get_num_atoms_of_type(m, "S") for m in molecules],
+        "n_phosphorus": [get_num_atoms_of_type(m, "P") for m in molecules],
+        "composition": [",".join(sorted(set([a.GetSymbol() for a in AddHs(m).GetAtoms()]))) for m in molecules]
     })
     merged_df = pd.merge(df, class_names, left_on=left_on, right_on='molname')
     return merged_df
@@ -104,7 +129,7 @@ def get_true_names(row, df):
     Returns:
     - list: List of true column names for the given row.
     """
-    return [col for col in df.columns[11:18] if row[col]]
+    return [col for col in df.columns[12:19] if row[col]]
 
 # Function to split values with commas and create new rows
 def split_and_add_rows(df, column_name, split_by):
@@ -153,11 +178,15 @@ def generate_combinations(df, column_name):
 
 def preprocess_data(merged_top5_same, cols_to_keep):
     key_cols = ['query', 'reference'] + cols_to_keep
+    
+    matches_col = 'CosineHungarian_0.01_0.0_1.0_matches'
+    scores_col = 'CosineHungarian_0.01_0.0_1.0_scores'
+    
     # Concatenate the DataFrames in df1_list and add a 'value' column with the value 'matches'.
-    df1 = merged_top5_same[key_cols + ['CosineHungarian_0.01_0.0_1.0_matches']].copy()
+    df1 = merged_top5_same[key_cols + [matches_col]].copy()
 
     # Concatenate the DataFrames in df2_list and add a 'value' column with the value 'scores'.
-    df2 = merged_top5_same[key_cols + ['CosineHungarian_0.01_0.0_1.0_scores']].copy()
+    df2 = merged_top5_same[key_cols + [scores_col]].copy()
 
     # Concatenate df1 and df2 into a single DataFrame.
     df_cat = pd.concat([df1, df2])
@@ -178,3 +207,29 @@ def load_spectra_metadata(file_path, metadata_column_name):
     spectra_metadata.rename(columns={'compound_name': metadata_column_name}, inplace=True)
     spectra_names = spectra_metadata[metadata_column_name].to_list()
     return spectra, spectra_metadata, spectra_names
+
+
+def append_spectrum_metadata(scores: pd.DataFrame):
+    _ , spectra_metadata, _ = load_spectra_metadata("../data/filtered/simulated_matchms_filter_1%I_all_peaks.msp", 'query')
+    _ , reference_spectra_metadata, _ = load_spectra_metadata("../data/experimental/RECETOX_GC-EI_MS_20201028.msp", 'reference')
+    merged = scores.merge(spectra_metadata, on="query", how="inner")
+    merged.rename(columns={'num_peaks': 'n_peaks_query'}, inplace=True)
+
+    merged = merged.merge(reference_spectra_metadata, on="reference", how="inner")
+    merged.rename(columns={'num_peaks': 'n_peaks_reference'}, inplace=True)
+
+    numeric_columns = ['CosineHungarian_0.01_0.0_1.0_matches', 'n_peaks_query', 'n_peaks_reference']
+    merged[numeric_columns] = merged[numeric_columns].apply(pd.to_numeric, errors='coerce')
+    return merged
+
+def normalize_df(df: pd.DataFrame, use_nist: bool = True, matches_norm_col: str = 'n_peaks_reference'):
+    matches_col = 'CosineHungarian_0.01_0.0_1.0_matches'
+    scores_col = 'CosineHungarian_0.01_0.0_1.0_scores'
+    
+    if use_nist:
+        df[scores_col] = df[scores_col] * 1000
+    
+    if matches_norm_col:
+        df[matches_col] = (df[matches_col] / df[matches_norm_col]) * 100
+    
+    return df
